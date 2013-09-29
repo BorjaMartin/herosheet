492aac5e55d733ac4ce489fa5e8a2a955052ac41
Borja Martin Fernandez <bmartinf@axpe.com>
2013-09-29 13:32:34 +0200

    Add basic authenticate controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,4 +1,5 @@
 class HerosController < ApplicationController
+  http_basic_authenticate_with :name => "admin", :password => "admin"
   before_action :set_hero, only: [:show, :edit, :update, :destroy]
 
   # GET /heros
