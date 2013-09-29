7f02c32f9167959a66a8f7f17002dbea4131d8d4
Borja Martin Fernandez <bmartinf@axpe.com>
2013-09-29 12:38:52 +0200

    Initialize herosheet in rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herosheet::Application.config.secret_key_base = 'f79b6e8051690f9eaba25d9ca2256db632e0447dc6c6d07b0c99f8df1e69926e98b5007368063c730fa686749488ac2e63a43e79c0aa2893022fd2046bc491b0'
